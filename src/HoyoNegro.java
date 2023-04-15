85f6157ec5bb6d6fa71f9d52931952f9a449173a
elchomps <114361109+elchomps@users.noreply.github.com>
2023-04-14 21:32:26 -0600

    creo la roomie hoyo negro

---
+++ src/HoyoNegro.java
@@ -0,0 +1,6 @@
+class HoyoNegro extends Espacio{
+  ////poner la variable de asteroides su tamaño////
+  HoyoNegro>TamanhoAsteriode
+ if (distancia>500 && distancia<350 && velocidad<3000) 
+   vida=vida-1000000
+}
