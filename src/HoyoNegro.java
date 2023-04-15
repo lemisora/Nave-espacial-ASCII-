6691ddeba8bb9bba6b565a844ca9bd3879fcff39
elchomps <114361109+elchomps@users.noreply.github.com>
2023-04-14 21:34:55 -0600

    punto y coma

--- src/HoyoNegro.java
+++ src/HoyoNegro.java
@@ -2,5 +2,5 @@ class HoyoNegro extends Espacio{
   ////poner la variable de asteroides su tamaño////
   //HoyoNegro>TamanhoAsteriode//
  if (distancia>500 && distancia<350 && velocidad<3000) 
-   vida=vida-1000000
+   vida=vida-1000000;
 }
