dd34bc7021a23bb362b1d7dddc7e3d3c44b3aa23
lemisora <leonardo.michel.dom@gmail.com>
2023-04-20 19:27:37 -0600

    Inclusión de los métodos y atributos de la Nave

--- src/changes-src/Nave.java
+++ src/changes-src/Nave.java
@@ -1,3 +1,45 @@
-class Nave {
+import java.util.Random;
+class Nave{
+  private String nombre;
+  private String nombrePiloto;
+  protected int velocidadMax;
+  protected int noBalas;
+  protected int vida;
+
+  public Nave(String nombre,String nombrePiloto){ //Constructor de la nave
+    this.nombre=nombre;
+    this.nombrePiloto=nombrePiloto;
+  }
+
+  public void Disparar(){
     
+  }
+
+  public void Desplazarse(){
+    
+  }
+
+  //Getters y setters del nombre
+  public void setNombre(String nombre){
+    this.nombre=nombre;
+  }
+  public String getNombre(){
+    return this.nombre;
+  }
+
+  //Getters y setters del nombre del nombrePiloto
+  public void setNombrePiloto(String nombrePiloto){
+    this.nombrePiloto=nombrePiloto;
+  }
+
+  public String getNombrePiloto(){
+    return this.nombrePiloto;
+  }
+
+  public void generaVelocidad(){
+    Random rand = new Random();
+    this.velocidadMax=rand.nextInt(500)+100;
+  }
+
+
 }
