c62abaf3196ac02ef457efeeb85445bcf621f370
lemisora <leonardo.michel.dom@gmail.com>
2023-04-20 20:52:42 -0600

    Atributos añadidos a las clases hijas de Nivel

--- src/changes-src/Niveles.java
+++ src/changes-src/Niveles.java
@@ -8,6 +8,10 @@ class Niveles {
     protected int nivel;
     protected boolean PASS;
     protected boolean sePuedeDisparar;
+
+    public Niveles(){       //Constructor de la clase
+
+    }
     
     public int determinaSiguienteNivel(){
         return nivel;
@@ -31,16 +35,31 @@ class Niveles {
 //                   CLASE DEL NIVEL 1                 //
 /////////////////////////////////////////////////////////
 class Niv1 extends Niveles{
+    protected int planetasSinVidaDestruidos;
+    protected boolean planetaConVidaDestruido;
+    public Niv1(){          //Constructor de la clase Niv1
+        super();
+    }
 }
 
 /////////////////////////////////////////////////////////
 //                  CLASE DEL NIVEL 2                  //
 /////////////////////////////////////////////////////////
 class Niv2 extends Niveles{
+    protected int noAsteroidesDestruidos;
+    protected boolean asteroideChocado;
+    public Niv2(){          //Constructor de la clase Niv2
+        super();
+    }
 }
 
 //////////////////////////////////////////////////////////
 //                  CLASE DEL NIVEL 3                   //
 //////////////////////////////////////////////////////////
 class Niv3 extends Niveles{
+    protected int noHoyosNegrosDestruidos;
+    protected boolean absorbido;            //Determina si es que se ha "chocado" con un hoyo negro
+    public Niv3(){                          //Constructor de la clase Niv3
+        super();
+    }
 }
